fc2992b75d8a46e7fa486774f60b28832c821e71
kalyanKumarPokkula <104988765+kalyanKumarPokkula@users.noreply.github.com>
2023-08-08 22:59:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.use("/me" , (req ,res ) => {
+  res.send("Iam P Kalyan kumar" );
+})
+
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
