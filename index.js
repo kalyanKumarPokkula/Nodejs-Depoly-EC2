6f9e70cdcc2bb5405095c272da5470be01d8a830
kalyanKumarPokkula <104988765+kalyanKumarPokkula@users.noreply.github.com>
2023-08-09 11:23:47 +0530

    add get route index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const PORT = 3000;
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello World");
-});
+
 
 app.use("/me" , (req ,res ) => {
   res.send("Iam P Kalyan kumar" );
 })
-
+app.get('/avi' , (req ,res) => {
+  res.send('avi is my best friend')
+})
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
